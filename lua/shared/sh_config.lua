5cdd07eec8b10fcabb9f8502fdda6e799ba3e2d3
ImpulseFPS <69686024+ImpulseFPS@users.noreply.github.com>
2021-12-29 06:21:59 +0100

    Update sh_config.lua

--- lua/shared/sh_config.lua
+++ lua/shared/sh_config.lua
@@ -4,15 +4,15 @@ SimpleBanking.Config = SimpleBanking.Config or {}
 
 SimpleBanking.Config["Days_Transaction_History"] = 5 -- How many days should the transaction history go back in the bank?
 
-SimpleBanking.Config["business_ranks_overrides"] = {
-    ["mechanic"] = { -- If you want a certain company to use custom job ranks, add them like below. otherwise, it defaults back to business_ranks
+SimpleBanking.Config["business_ranks_overrides"] = { -- Grade names must be lower case!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ["mechanic"] = { 
     ["RANK-NAME"] = true,
         ["RANK-NAME"] = true,
         ["RANK-NAME"] = true,
     },
     ['burgershot'] = {
-        ['kuhar'] = true,
-        ['zamjenik šefa'] = true,
+        ["RANK-NAME"] = true,
+        ["RANK-NAME"] = true,
     }
 }
 
