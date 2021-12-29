c243a757cbc2eba640faa4a4deda2e684d3739e0
ImpulseFPS <69686024+ImpulseFPS@users.noreply.github.com>
2021-12-29 06:12:49 +0100

    Example

--- lua/shared/sh_config.lua
+++ lua/shared/sh_config.lua
@@ -6,9 +6,9 @@ SimpleBanking.Config["Days_Transaction_History"] = 5 -- How many days should the
 
 SimpleBanking.Config["business_ranks_overrides"] = {
     ["mechanic"] = { -- If you want a certain company to use custom job ranks, add them like below. otherwise, it defaults back to business_ranks
-        ["owner"] = true,
-        ["coowner"] = true,
-        ["manager"] = true,
+    ["RANK-NAME"] = true,
+        ["RANK-NAME"] = true,
+        ["RANK-NAME"] = true,
     },
     ['burgershot'] = {
         ['kuhar'] = true,
@@ -17,4 +17,8 @@ SimpleBanking.Config["business_ranks_overrides"] = {
 }
 
 SimpleBanking.Config["gang_ranks_overrides"] = {
+    ['lostmc'] = {
+        ["RANK-NAME"] = true,
+        ["RANK-NAME"] = true,
+    }
 }
