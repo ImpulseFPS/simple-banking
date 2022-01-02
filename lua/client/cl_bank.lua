1a4afc65415dda4bf91fde494f4eed8404d7996a
DaShizz <toofy0@yahoo.com>
2022-01-02 00:05:30 -0500

    woops.
    forgot debug poly on + changed bank icon

--- lua/client/cl_bank.lua
+++ lua/client/cl_bank.lua
@@ -121,7 +121,7 @@ exports['qb-target']:AddTargetModel(-1364697528,  {
 exports['qb-target']:AddBoxZone("Bank1", vector3(149.07, -1041.16, 29.54), 1.5, 4.5, {
 	name = "Bank1",
 	heading = 160,
-	debugPoly = true,
+	debugPoly = false,
 	minZ = 28,
 	maxZ = 30.5,
 	}, {
@@ -129,7 +129,7 @@ exports['qb-target']:AddBoxZone("Bank1", vector3(149.07, -1041.16, 29.54), 1.5, 
 			{
             	type = "client",
             	event = "qb-banking:client:bank:openUI",
-				icon = "fas fa-sign-in-alt",
+				icon = "fas fa-piggy-bank",
 				label = "Sign In",
 			},
 		},
@@ -139,7 +139,7 @@ exports['qb-target']:AddBoxZone("Bank1", vector3(149.07, -1041.16, 29.54), 1.5, 
 exports['qb-target']:AddBoxZone("Bank2", vector3(-1212.98, -331.53, 38.24), 1.5, 4.5, {
 	name = "Bank2",
 	heading = 206,
-	debugPoly = true,
+	debugPoly = false,
 	minZ = 37,
 	maxZ = 39,
 	}, {
@@ -147,7 +147,7 @@ exports['qb-target']:AddBoxZone("Bank2", vector3(-1212.98, -331.53, 38.24), 1.5,
 			{
             	type = "client",
             	event = "qb-banking:client:bank:openUI",
-				icon = "fas fa-sign-in-alt",
+				icon = "fas fa-piggy-bank",
 				label = "Sign In",
 			},
 		},
@@ -157,7 +157,7 @@ exports['qb-target']:AddBoxZone("Bank2", vector3(-1212.98, -331.53, 38.24), 1.5,
 exports['qb-target']:AddBoxZone("Bank3", vector3(-2962.16, 482.17, 15.7), 1.5, 4.5, {
 	name = "Bank3",
 	heading = 269,
-	debugPoly = true,
+	debugPoly = false,
 	minZ = 15,
 	maxZ = 18,
 	}, {
@@ -165,7 +165,7 @@ exports['qb-target']:AddBoxZone("Bank3", vector3(-2962.16, 482.17, 15.7), 1.5, 4
 			{
             	type = "client",
             	event = "qb-banking:client:bank:openUI",
-				icon = "fas fa-sign-in-alt",
+				icon = "fas fa-piggy-bank",
 				label = "Sign In",
 			},
 		},
@@ -175,7 +175,7 @@ exports['qb-target']:AddBoxZone("Bank3", vector3(-2962.16, 482.17, 15.7), 1.5, 4
 exports['qb-target']:AddBoxZone("Bank4", vector3(-112.29, 6469.38, 31.63), 1.5, 4.5, {
 	name = "Bank4",
 	heading = 316,
-	debugPoly = true,
+	debugPoly = false,
 	minZ = 31,
 	maxZ = 33,
 	}, {
@@ -183,7 +183,7 @@ exports['qb-target']:AddBoxZone("Bank4", vector3(-112.29, 6469.38, 31.63), 1.5, 
 			{
             	type = "client",
             	event = "qb-banking:client:bank:openUI",
-				icon = "fas fa-sign-in-alt",
+				icon = "fas fa-piggy-bank",
 				label = "Sign In",
 			},
 		},
@@ -193,7 +193,7 @@ exports['qb-target']:AddBoxZone("Bank4", vector3(-112.29, 6469.38, 31.63), 1.5, 
 exports['qb-target']:AddBoxZone("Bank5", vector3(313.56, -279.7, 54.8), 1.5, 4.5, {
 	name = "Bank5",
 	heading = 160,
-	debugPoly = true,
+	debugPoly = false,
 	minZ = 53,
 	maxZ = 55,
 	}, {
@@ -201,7 +201,7 @@ exports['qb-target']:AddBoxZone("Bank5", vector3(313.56, -279.7, 54.8), 1.5, 4.5
 			{
             	type = "client",
             	event = "qb-banking:client:bank:openUI",
-				icon = "fas fa-sign-in-alt",
+				icon = "fas fa-piggy-bank",
 				label = "Sign In",
 			},
 		},
@@ -211,7 +211,7 @@ exports['qb-target']:AddBoxZone("Bank5", vector3(313.56, -279.7, 54.8), 1.5, 4.5
 exports['qb-target']:AddBoxZone("Bank6", vector3(-351.51, -49.8, 49.04), 1.5, 4.5, {
 	name = "Bank6",
 	heading = 160,
-	debugPoly = true,
+	debugPoly = false,
 	minZ = 49,
 	maxZ = 50,
 	}, {
@@ -219,7 +219,7 @@ exports['qb-target']:AddBoxZone("Bank6", vector3(-351.51, -49.8, 49.04), 1.5, 4.
 			{
             	type = "client",
             	event = "qb-banking:client:bank:openUI",
-				icon = "fas fa-sign-in-alt",
+				icon = "fas fa-piggy-bank",
 				label = "Sign In",
 			},
 		},
@@ -229,7 +229,7 @@ exports['qb-target']:AddBoxZone("Bank6", vector3(-351.51, -49.8, 49.04), 1.5, 4.
 exports['qb-target']:AddBoxZone("Bank7", vector3(1175.92, 2707.86, 38.09), 1.5, 4.5, {
 	name = "Bank7",
 	heading = 359.73,
-	debugPoly = true,
+	debugPoly = false,
 	minZ = 37,
 	maxZ = 39,
 	}, {
@@ -237,7 +237,7 @@ exports['qb-target']:AddBoxZone("Bank7", vector3(1175.92, 2707.86, 38.09), 1.5, 
 			{
             	type = "client",
             	event = "qb-banking:client:bank:openUI",
-				icon = "fas fa-sign-in-alt",
+				icon = "fas fa-piggy-bank",
 				label = "Sign In",
 			},
 		},
@@ -247,7 +247,7 @@ exports['qb-target']:AddBoxZone("Bank7", vector3(1175.92, 2707.86, 38.09), 1.5, 
 exports['qb-target']:AddBoxZone("BigBank", vector3(242.41, 225.03, 106.29), 1.5, 4.5, {
 	name = "BigBank",
 	heading = 342.44,
-	debugPoly = true,
+	debugPoly = false,
 	minZ = 105,
 	maxZ = 108,
 	}, {
@@ -255,7 +255,7 @@ exports['qb-target']:AddBoxZone("BigBank", vector3(242.41, 225.03, 106.29), 1.5,
 			{
             	type = "client",
             	event = "qb-banking:client:bank:openUI",
-				icon = "fas fa-sign-in-alt",
+				icon = "fas fa-piggy-bank",
 				label = "Sign In",
 			},
 		},
